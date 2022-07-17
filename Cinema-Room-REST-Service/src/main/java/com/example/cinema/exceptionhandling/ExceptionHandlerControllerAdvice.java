90f9ae57a4e1b78dcd779763285e1b324d0af734
tkim013 <tkim013@gmail.com>
2022-07-17 08:57:26 -0400

    update

---
+++ Cinema-Room-REST-Service/src/main/java/com/example/cinema/exceptionhandling/ExceptionHandlerControllerAdvice.java
@@ -0,0 +1,55 @@
+package com.example.cinema.exceptionhandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.naming.AuthenticationException;
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ExceptionHandlerControllerAdvice {
+
+    @ExceptionHandler(NumberOutOfBoundsException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public @ResponseBody ExceptionResponse handleNumberOutOfBoundsException(final Exception exception,
+                                                           final HttpServletRequest request) {
+
+        ExceptionResponse error = new ExceptionResponse();
+        error.setError(exception.getMessage());
+        return error;
+    }
+
+    @ExceptionHandler(SeatUnavailableException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public @ResponseBody ExceptionResponse handleSeatUnavailableException(final Exception exception,
+                                                                            final HttpServletRequest request) {
+
+        ExceptionResponse error = new ExceptionResponse();
+        error.setError(exception.getMessage());
+        return error;
+    }
+
+    @ExceptionHandler(InvalidTokenException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public @ResponseBody ExceptionResponse handleInvalidTokenException(final Exception exception,
+                                                                          final HttpServletRequest request) {
+
+        ExceptionResponse error = new ExceptionResponse();
+        error.setError(exception.getMessage());
+        return error;
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    public @ResponseBody ExceptionResponse handleAuthenticationException(final Exception exception,
+                                                           final HttpServletRequest request) {
+
+        ExceptionResponse error = new ExceptionResponse();
+        error.setError(exception.getMessage());
+        return error;
+    }
+
+}
